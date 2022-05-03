70028b2a2467ba146bb426e7fc66b2c80072b3b5
hyeonsik <dgt4862@gmail.com>
2022-05-03 21:24:51 +0900

    navigation 활용 및 델리게이트 사용

---
+++ Navigation_Mission/EditViewController.swift
@@ -0,0 +1,61 @@
+//
+//  EditViewController.swift
+//  Navigation_Mission
+//
+//  Created by hyeonsik on 2022/05/03.
+//
+
+import UIKit
+
+protocol EditDelegate {
+    func didMessageEditDone(_ controller: EditViewController, message: String)
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool)
+}
+
+class EditViewController: UIViewController {
+    var textWayValue: String = ""
+    var textMessage: String = ""
+    var delegate : EditDelegate?
+    var isOn = false
+
+    @IBOutlet var lblWay: UILabel!
+    @IBOutlet var txMessage: UITextField!
+    @IBOutlet var swlsOn: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        lblWay.text = textWayValue
+        txMessage.text = textMessage
+        swlsOn.isOn = isOn
+    }
+    
+    @IBAction func bntDone(_ sender: UIButton) {
+        if delegate != nil {
+            delegate?.didMessageEditDone(self, message: txMessage.text!)
+            delegate?.didImageOnOffDone(self, isOn: isOn)
+        }
+        _ = navigationController?.popViewController(animated: true)
+    }
+    @IBAction func swImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            isOn = true
+        }
+        else {
+            isOn = false
+        }
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Navigation_Mission/ViewController.swift
+++ Navigation_Mission/ViewController.swift
@@ -7,12 +7,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, EditDelegate {
+    let imgOn = UIImage(named: "lamp-on.png")
+    let imgOff = UIImage(named: "lamp-off.png")
+    
+    var isOn = true
+    
+    @IBOutlet var txMessage: UITextField!
+    @IBOutlet var imgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imgView.image = imgOn
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // 세그웨이를 이용하여 화면을 전환하기 위해 사용되는 함수
+        let editViewController = segue.destination as! EditViewController
+        if segue.identifier == "editButton" {
+            editViewController.textWayValue = "segue : use Button"
+        }
+        else if segue.identifier == "editBarButton" {
+            editViewController.textWayValue = "segue : use Bar Button"
+        }
+        
+        editViewController.textMessage = txMessage.text! // 메인 -> 수정 화면으로 데이터 이동
+        editViewController.isOn = isOn // 전구의 상태를 전달
+        editViewController.delegate = self
+    }
+    
+    func didMessageEditDone(_ controller: EditViewController, message: String) {
+        txMessage.text = message
+    }
+    
+    func didImageOnOffDone(_ controller: EditViewController, isOn: Bool) {
+        if isOn {
+            imgView.image = imgOn
+            self.isOn = true
+        }
+        else {
+            imgView.image = imgOff
+            self.isOn = false
+        }
+    }
+    
 
 
 }
